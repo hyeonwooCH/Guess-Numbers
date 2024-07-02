b78252d3d333268a9ec7b6c14f6ee383a88c18ac
최현우 <115483654+hyeonwooCH@users.noreply.github.com>
2024-07-02 21:39:18 +0900

    Delete start directory

--- start/python 기본 개념.py
+++
@@ -1,5 +0,0 @@
-x = 5 # 숫자형 변수
-y = "Hello Python!" # 문자형 변수
-
-print(x)
-print(y)
--- start/객체 지향 Programming.py
+++
@@ -1,9 +0,0 @@
-class Dog:
-    def __init__(self, name):
-        self.name = name
-        
-    def bark(self):
-        return f"{self.name} says woof!"
-    
-my_dog = Dog("Buddy")
-print(my_dog.bark())
